9275557809291483090dd77fb9974e5561b0d7bf
Edwards Presume <edwardspresume@gmail.com>
2024-05-16 17:48:27 -0400

    Add enhancedImages and kitRoutes plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { kitRoutes } from 'vite-plugin-kit-routes';
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [enhancedImages(), sveltekit(), kitRoutes()]
 });
