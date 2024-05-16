a4c6c921e8dffdb625306dd9b5ae895aa79b6e85
Edwards Presume <edwardspresume@gmail.com>
2024-05-16 17:59:56 -0400

    Update layout component with navigation links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,45 @@
 <script>
-	import Button from '$lib/components/ui/button/button.svelte';
+	import { page } from '$app/stores';
+
+	import { siteLinks } from '$lib/navLinks';
+
 	import '../app.css';
+
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	const { children } = $props();
 </script>
 
-<main class="container py-6">
-	<nav class="mb-5 flex justify-center space-x-4">
-		<Button href="/">Home</Button>
-		<Button href="/gallery">Gallery</Button>
-	</nav>
+<header class="relative mb-10 border-b border-accent-foreground/10">
+	<div class="container flex items-center justify-between p-2">
+		<Button href="/" variant="link" class="p-0 text-2xl font-bold">VisionVault</Button>
 
+		<nav class="flex gap-2">
+			{#each Object.values(siteLinks) as link}
+				{@const isCurrentPage = $page.url.pathname === link.href ? 'page' : undefined}
+				<Button
+					href={link.href}
+					size="sm"
+					variant="outline"
+					aria-label={link.ariaLabel}
+					aria-current={isCurrentPage}
+					class="hover:bg-accent-foreground hover:text-background"
+				>
+					{link.title}
+				</Button>
+			{/each}
+		</nav>
+	</div>
+</header>
+
+<main class="container">
 	{@render children()}
 </main>
+
+<style lang="postcss">
+	nav :global {
+		a[aria-current='page'] {
+			@apply bg-accent-foreground text-background;
+		}
+	}
+</style>
